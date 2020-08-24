589a811cf0fb8b49431cf75122a1108a2997aadd
Patel Krushankant <patelkk1618@gmail.com>
2020-08-24 09:54:25 -0500

    Save Data To Local Storage

--- 3. Movie-Seat-Booking/appScript.js
+++ 3. Movie-Seat-Booking/appScript.js
@@ -7,10 +7,21 @@ const movieSelect  = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 
 //update total and count
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    // copy selected seats into array
+    // Map through array
+    // return a new array indexes.
+    const seatsIndex =[...selectedSeats].map(seat => [...seats].indexOf(seat));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
     const selectedSeatsCount = selectedSeats.length;
 
@@ -28,6 +39,7 @@ container.addEventListener('click', e=>{
 
 // Movie Select event
 movieSelect.addEventListener('change', e=>{
-    ticketPrice+=e.target.value;
+    ticketPrice= +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
