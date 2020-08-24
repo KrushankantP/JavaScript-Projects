e3b45d23d3b5249a45083f06308a6c967099ca08
Patel Krushankant <patelkk1618@gmail.com>
2020-08-24 11:51:00 -0500

    Populated UI with saved data.

--- 3. Movie-Seat-Booking/appScript.js
+++ 3. Movie-Seat-Booking/appScript.js
@@ -4,6 +4,7 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect  = document.getElementById('movie');
 
+populateUI();
 
 let ticketPrice = +movieSelect.value;
 
@@ -20,13 +21,35 @@ function updateSelectedCount(){
     // copy selected seats into array
     // Map through array
     // return a new array indexes.
-    const seatsIndex =[...selectedSeats].map(seat => [...seats].indexOf(seat));
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    // To SetItem to localstorage as in string by JSON.stringify.
     localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
+
+}
+
+// Get data from localStorage and populate UI
+function populateUI(){
+
+    //getItems from the local storage and converted into its original array's by using JSON.parse
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats!== null && selectedSeats.length > 0){
+        seats.forEach((seat, index)=>{
+            // (if there is no index that we are looking for than it will return -1) and index always start from 0,1,2,3...
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected');
+            }
+        });
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if(selectedMovieIndex !== null){
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
 }
 
 //seat Click event
@@ -43,3 +66,6 @@ movieSelect.addEventListener('change', e=>{
     setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
+
+//Initial count and total set (called on page load first)
+updateSelectedCount()
